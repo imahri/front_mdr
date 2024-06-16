9ef37442f4e05f3cc0acd38300723fdf53ed360a
Mahri Imad-Eddine <imahri@student.1337.ma>
2024-06-16 18:02:56 +0100

    add close icon

--- space_pong_to_do/app/auth/components/login_top.jsx
+++ space_pong_to_do/app/auth/components/login_top.jsx
@@ -3,9 +3,11 @@ import style from "./login.module.css"
 import Image from "next/image";
 import li from "../../picture/fline.png"
 import logo from "../../picture/logo.png"
+import { IoClose } from "react-icons/io5";
 
 const TopLogin = () => {
     return <div className={`${style.layer} ${style.container_logo}`}>
+            <IoClose className={style.closed} size={35}/>
             <div className={style.line}>
                 <Image
                     src={li}
