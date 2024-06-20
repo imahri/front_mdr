41c862f9e9cc1ce4d15dd42865863b67b5b6f659
Mahri Imad-Eddine <imahri@student.1337.ma>
2024-06-20 21:42:41 +0100

    dashboard 90%

--- space_pong_to_do/app/dashboard/components/choices.jsx
+++ space_pong_to_do/app/dashboard/components/choices.jsx
@@ -1,35 +1,41 @@
 "use client"
 import style from "./choices.module.css"
 import Image from "next/image";
-import bg1 from "../../picture/hard_card/trf1.png"
-import bg2 from "../../picture/hard_card/trf2.png"
-import bg3 from "../../picture/hard_card/trf3.png"
+// import bg1 from "../../picture/hard_card/trf1.png"
+// import bg2 from "../../picture/hard_card/trf2.png"
+// import bg3 from "../../picture/hard_card/trf3.png"
+import bg1 from "../../picture/hard_card/one.png"
+import bg2 from "../../picture/hard_card/two.png"
+import bg3 from "../../picture/hard_card/three.png"
 
 const Choices = () => {
     return <div className={style.container}>
-        <div>
+        <div className={style.base}>
             <Image 
                 src={bg1}
                 width={500}
                 height={500}
                 className={`${style.box1} ${style.box}`}
             />
+            <p className={`${style.des} ${style.tour}`}>TOURNAMENTS</p>
         </div>
-        <div>
+        <div className={style.base}>
             <Image 
                 src={bg2}
                 width={500}
                 height={500}
                 className={`${style.box2} ${style.box}`}
             />
+            <p className={`${style.des} ${style.train}`}>TRAINING</p>
         </div>
-        <div>
+        <div className={style.base}>
             <Image 
                 src={bg3}
                 width={500}
                 height={500}
                 className={`${style.box3} ${style.box}`}
             />
+            <p className={`${style.des} ${style.vs}`}>1 VS 1</p>
         </div>
     </div>;
 }
